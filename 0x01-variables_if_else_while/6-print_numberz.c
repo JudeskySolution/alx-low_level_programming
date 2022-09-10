c04ba962320bf86f766961e8c1cb7f7e9ff4d0ee
JudeskySolution <ezejude069@gmail.com>
2022-09-09 20:35:10 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+
+/**
+*main-Entry point
+*
+*Return: Always 0 (Success)     
+*/
+int main(void)
+{
+		int d = 48;
+		
+		while (d < 58)
+		{
+			putchar(d);
+		d++;
+		}
+		putchar('\n');
+
+		return (0);
+}
