4969a7c81623a5ef75487ec941de1ba170f0db14
JudeskySolution <ezejude069@gmail.com>
2022-09-13 16:43:50 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	char c[] = "_putchar";
+	char a[] = "_putchar";
 	int b;
 
 	for (b = 0; b < 8; b++)
 	{
-		_putchar(c[b]);
+		_putchar(a[b]);
 	}
 		_putchar('\n');
 		return (0);
