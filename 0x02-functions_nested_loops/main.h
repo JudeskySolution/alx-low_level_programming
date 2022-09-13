cb38b9cf2ff516c8a60c5d1f1d91060a091cb8f2
JudeskySolution <ezejude069@gmail.com>
2022-09-13 16:08:18 -0700

    Print _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,17 @@
- 1 #ifndef MAIN_H
- 2 #define MAIN_H
- 3
- 4 int _putchar(char c);
- 5 void print_alphabet(void);
- 6 void print_alphabet_x10(void);
- 7 int _islower(int c);
- 8 int _isalpha(int c);
- 9 int print_sign(int n);
-10 int _abs(int n);
-11 int print_last_digit(int n);
-12 void jack_bauer(void);
-13 void times_table(void);
-14 int add(int, int);
-15 void print_to_98(int n);
-16
-17 #endif
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c)
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif
