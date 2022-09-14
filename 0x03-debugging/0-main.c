60e11d0633fb36b2415d4bd06fa65533de6e8384
JudeskySolution <ezejude069@gmail.com>
2022-09-14 09:10:02 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <time.h>
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -10,13 +9,8 @@ int main(void)
 {
 	int i;
 
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else
-		printf("%d is negative\n", i);
+	i = 98;
+	positive_negative(i);
 
 	return (0);
 }
